8f3f130668c64a3e055e151479a9becd11f67528
huy <huylexuan2911@gmail.com>
2025-12-13 16:15:37 +0700

    init setup java maven

---
+++ src/main/java/com/CS445/CS4445_Sub_Server/Cs4445SubServerApplication.java
@@ -0,0 +1,13 @@
+package com.CS445.CS4445_Sub_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4445SubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4445SubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CS445/CS4445_Sub_Server/Cs4445SubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS445.CS4445_Sub_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4445SubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
